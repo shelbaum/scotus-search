45894c76a78bf3c872cc866ef5037950a84ce50d
shelbaum <45436319+shelbaum@users.noreply.github.com>
2020-10-15 17:27:33 -0700

    Update search_app.py

--- search_app.py
+++ search_app.py
@@ -20,7 +20,7 @@ search_terms = ['Ablavsky', 'Alexander', 'Anderson', 'Babcock', 'Bankman', 'Bank
 'Simon', 'Sinnar', 'Sivas', 'Sklansky', 'Sonne', 'Spaulding', 'Srikantiah', 'Strnad', 'Studdert', 'Sykes', 
 'Thompson', 'Triantis', 'Tyler', 'Schewick', 'Wald\\b', 'Weiner', 'Weisberg', 'Williams', 'Zambrano', 'Broyde', 
 'Bertran', 'Cuéllar', 'Feingold', 'Ferrell', '\\bFina\\b', 'Fletcher', 'Hemel', 'Hodrick', 'Huq', 'Mack\b', 'Perdomo', 
-'Schaack', 'Welton', 'Stanford', 'Stan\.', '[Rr]eply', '[Rr]esponse', '[Aa]micus', '[Bb]rief\\b', 'CA10']
+'Schaack', 'Welton', 'Stanford', 'Stan\.', '[Rr]eply', '\\b[Rr]esponse\\b', '[Aa]micus', '[Bb]rief\\b', 'CA10']
 
 class DateForm(FlaskForm):
 	start_date = DateField("From: ", format='%Y-%m-%d', validators=[DataRequired()])
